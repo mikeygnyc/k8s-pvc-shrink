798f3037f5607ea1222f7cd3c155c8d7b45766de
Mike Gales <github@galesnoc.com>
2026-03-01 19:42:51 -0600

    feat: add check dependencies ci: cleanup test/release logic

--- intstall.sh
+++ intstall.sh
@@ -6,7 +6,6 @@ REPO_NAME="k8s-pvc-shrink"
 TARGET="kubectl-pvc-shrink"
 RAW_URL="https://raw.githubusercontent.com/$REPO_USER/$REPO_NAME/main"
 
-# Detect Path
 if command -v brew >/dev/null 2>&1; then
     BIN_DIR="$(brew --prefix)/bin"
 else
@@ -15,17 +14,12 @@ fi
 
 echo "🚀 Installing $TARGET to $BIN_DIR..."
 
-# Added quotes to variables below to satisfy SC2086
 if [[ -f "./$TARGET" ]]; then
     cp "./$TARGET" "$BIN_DIR/$TARGET"
 else
     curl -fsSL "$RAW_URL/$TARGET" -o "$BIN_DIR/$TARGET"
 fi
 
-if [ -w "$BIN_DIR" ]; then
-    chmod +x "$BIN_DIR/$TARGET"
-else
-    sudo chmod +x "$BIN_DIR/$TARGET"
-fi
+chmod +x "$BIN_DIR/$TARGET"
 
 echo "✅ Installed! Run 'kubectl pvc-shrink' to start."
